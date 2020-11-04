221c2b3b5c2b47bb59c8fb50346e18116bedc524
Martin Pradny <pradny@pris.to>
2020-11-04 21:23:03 +0100

    Structured definition of Domino configuration

--- src/main/kotlin/net/pristo/kotlin/proton/ApiDemoApplication.kt
+++ src/main/kotlin/net/pristo/kotlin/proton/ApiDemoApplication.kt
@@ -1,10 +1,13 @@
 package net.pristo.kotlin.proton
 
+import net.pristo.kotlin.proton.config.DominoProperties
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(DominoProperties::class)
 class ApiDemoApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/net/pristo/kotlin/proton/config/Domino.kt
+++ src/main/kotlin/net/pristo/kotlin/proton/config/Domino.kt
@@ -3,48 +3,58 @@ package net.pristo.kotlin.proton.config
 import com.hcl.domino.db.model.Database
 import com.hcl.domino.db.model.Server
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
-import org.springframework.core.env.getProperty
 import java.io.File
 import java.net.URLDecoder
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+@ConstructorBinding
+data class DominoSecurity(
+    val trustFile: String,
+    val certFile: String,
+    val keyFile: String,
+    val keyPassword: String?,
+    val idPassword: String?
+)
+
+@ConstructorBinding
+@ConfigurationProperties("domino")
+data class DominoProperties(
+    val executors: Int = 1,
+    val server: String,
+    val port: Int,
+    val database: String,
+    val security : DominoSecurity
+)
 
 @Configuration
 class DominoConfig(
-    val env: Environment
+    val dominoProperties: DominoProperties
 ) {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun getExecutorService(): ExecutorService {
-        val executors = env.getProperty<Int>("domino.executors") ?: 1
+        val executors = dominoProperties.executors
         return Executors.newFixedThreadPool(executors)
     }
 
     @Bean
     fun getDatabase(executorService: ExecutorService): Database {
         log.debug("Initializing Domino connection")
-        val hostName = env.getProperty("domino.server") ?: throw IllegalArgumentException("server hostname not specified")
-        val port = env.getProperty<Int>("domino.port") ?: throw IllegalArgumentException("server port not specified")
-        val database = env.getProperty("domino.database") ?: throw IllegalArgumentException("database not specified")
-        val trustFile = env.getProperty("domino.security.trustFile") ?: throw IllegalArgumentException("trust file not specified")
-        val certFile = env.getProperty("domino.security.certFile") ?: throw IllegalArgumentException("cert file not specified")
-        val keyFile = env.getProperty("domino.security.keyFile") ?: throw IllegalArgumentException("key file not specified")
-        val idPassword = env.getProperty("domino.security.idPassword") ?: throw IllegalArgumentException("id password")
-
 
         val server = Server(
-            hostName, port, getFile(trustFile), getFile(certFile),
-            getFile(keyFile), null, idPassword, executorService
+            dominoProperties.server, dominoProperties.port, getFile(dominoProperties.security.trustFile), getFile(dominoProperties.security.certFile),
+            getFile(dominoProperties.security.keyFile), dominoProperties.security.keyPassword, dominoProperties.security.idPassword, executorService
         )
 
         log.debug("Done initializing Domino connection")
-        return server.useDatabase(database)
+        return server.useDatabase(dominoProperties.database)
     }
 
     private fun getFile(fileName: String): File {
